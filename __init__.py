6101fbca46df72b01e9a3625f3c31fa370d93de7
Zashel <ivan.uria@gmail.com>
2017-04-05 12:11:53 +0200

    Debug

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
 import os
-from .zrest import *
+__path__[0] = os.path.join(__path__[0], "zrest/")
+#from .zrest import basedatamodel
+#from .zrest import exceptions
+#from .zrest import server
+#from .zrest import statuscodes
+#from .zrest import datamodels
 
-__path__[0] = os.path.join(__path__[0], "zrest")
+#
 
